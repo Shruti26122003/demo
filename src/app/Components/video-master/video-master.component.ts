a44db8b01232d140576720d137eb1f26be433b63
Shruti26122003 <shrutim8141@gmail.com>
2024-12-02 22:29:59 +0530

    Angular Learning Process

---
+++ src/app/Components/video-master/video-master.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-master',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-master.component.html',
+  styleUrl: './video-master.component.css'
+})
+export class VideoMasterComponent {
+  
+}
